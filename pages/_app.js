091293f7078149937678f964261ee117116ef56b
Owais Ahmad <m.owais4014@gmail.com>
2022-04-14 06:01:14 +0500

    feat: add first page content and styling

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,23 @@
-import '../styles/globals.css'
+import { ChakraProvider, extendTheme } from '@chakra-ui/react';
+const theme = extendTheme({
+
+  fonts: {
+    heading: 'Poppins, sans-serif',
+    body: 'Raleway, sans-serif'
+  },
+  styles: {
+    global: {
+      body: {
+        bg: 'blue.200',
+        color: 'black'
+      }
+    }
+  }
+})
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+
+  return <ChakraProvider theme={theme}><Component {...pageProps} /></ChakraProvider>
 }
 
 export default MyApp
